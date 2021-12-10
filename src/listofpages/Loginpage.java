3106ff76cdff621359956c7c4bdc84c2c22a4be4
unknown <rrameshsruthik@gmail.com>
2021-12-10 12:32:10 +0530

    Login code added

---
+++ src/listofpages/Loginpage.java
@@ -0,0 +1,12 @@
+package listofpages;
+
+public class Loginpage {
+	
+	public void employeeName() {
+		
+		
+		System.out.println("Ramesh");
+	}
+	
+
+}
---
+++ src/listoftest/LoginpageTest.java
@@ -0,0 +1,12 @@
+package listoftest;
+
+import listofpages.Loginpage;
+
+public class LoginpageTest {
+
+	
+	public static void main(String[] args) {
+		Loginpage log=new Loginpage();
+		log.employeeName();
+	}
+}
